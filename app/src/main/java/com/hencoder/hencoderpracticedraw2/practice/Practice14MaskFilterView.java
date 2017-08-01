04e433e019aa3841d1379ccab1849d3bcc8ef472
fengmlo <fengmlo@gmail.com>
2017-08-01 19:05:03 +0800

    finish practice

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice14MaskFilterView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice14MaskFilterView.java
@@ -3,9 +3,9 @@ package com.hencoder.hencoderpracticedraw2.practice;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -15,8 +15,12 @@ import com.hencoder.hencoderpracticedraw2.R;
 public class Practice14MaskFilterView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Bitmap bitmap;
+	private BlurMaskFilter blurMaskFilter1 = new BlurMaskFilter(50, BlurMaskFilter.Blur.NORMAL);
+	private BlurMaskFilter blurMaskFilter2  = new BlurMaskFilter(50, BlurMaskFilter.Blur.INNER);
+	private BlurMaskFilter blurMaskFilter3  = new BlurMaskFilter(50, BlurMaskFilter.Blur.OUTER);
+	private BlurMaskFilter blurMaskFilter4  = new BlurMaskFilter(50, BlurMaskFilter.Blur.SOLID);
 
-    public Practice14MaskFilterView(Context context) {
+	public Practice14MaskFilterView(Context context) {
         super(context);
     }
 
@@ -40,15 +44,19 @@ public class Practice14MaskFilterView extends View {
         // 用 Paint.setMaskFilter 来设置不同的 BlurMaskFilter
 
         // 第一个：NORMAL
+	    paint.setMaskFilter(blurMaskFilter1);
         canvas.drawBitmap(bitmap, 100, 50, paint);
 
         // 第二个：INNER
-        canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, 50, paint);
+	    paint.setMaskFilter(blurMaskFilter2);
+	    canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, 50, paint);
 
         // 第三个：OUTER
-        canvas.drawBitmap(bitmap, 100, bitmap.getHeight() + 100, paint);
+	    paint.setMaskFilter(blurMaskFilter3);
+	    canvas.drawBitmap(bitmap, 100, bitmap.getHeight() + 100, paint);
 
         // 第四个：SOLID
-        canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, bitmap.getHeight() + 100, paint);
+	    paint.setMaskFilter(blurMaskFilter4);
+	    canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, bitmap.getHeight() + 100, paint);
     }
 }
